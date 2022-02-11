db919a8f9202a680ddd80c50cfe59571cb2a181c
zxbuzzy <zxbuzzy@gmail.com>
2022-02-11 14:25:51 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Никита Андриянников on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
