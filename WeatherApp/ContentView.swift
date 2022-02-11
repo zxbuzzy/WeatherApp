b192fcbf4ff567cc461753c75e80d60823861c58
zxbuzzy <zxbuzzy@gmail.com>
2022-02-11 14:44:22 +0300

    initial commit

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
